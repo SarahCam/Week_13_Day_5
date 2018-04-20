7b20a28972b7391f3ef510864b67b3a9d3b1051e
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-20 11:02:22 +0100

    Added simple tess for subtract and multiply

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -4,14 +4,6 @@
 // You need to write unit tests to ensure that the majority of functions in the calculator operate correctly. The test framework to be used is Mocha. The file to write in is in `/tests/unit/calculator_spec.js`.
 //
 // All of these functions should be tested thoroughly:
-//
-//   - calculator.add()
-//   - calculator.subtract()
-//   - calculator.multiply()
-//   - calculator.divide()
-//   - calculator.numberClick()
-//   - calculator.operatorClick()
-//   - calculator.clearClick()
 
 var Calculator = require('../../public/js/calculator.js')
 var assert = require('assert')
@@ -26,6 +18,7 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  //   - calculator.add()
   it('can add', function(){
     calculator.clearClick();
     calculator.numberClick(2);
@@ -35,5 +28,30 @@ describe('calculator', function () {
     calculator.operatorClick("=");
     assert.equal(25, calculator.runningTotal);
   })
+
+  //   - calculator.subtract()
+  it('can subtract', function(){
+    calculator.clearClick();
+    calculator.numberClick(4);
+    calculator.operatorClick("-");
+    calculator.numberClick(2);
+    calculator.operatorClick("=");
+    assert.equal(2, calculator.runningTotal);
+  })
+
+  //   - calculator.multiply()
+  it('can multiply', function(){
+    calculator.clearClick();
+    calculator.numberClick(4);
+    calculator.operatorClick("*");
+    calculator.numberClick(2);
+    calculator.operatorClick("=");
+    assert.equal(8, calculator.runningTotal);
+  })
+  //   - calculator.divide()
+  //   - calculator.numberClick()
+  //   - calculator.operatorClick()
+  //   - calculator.clearClick()
+
 
 });
