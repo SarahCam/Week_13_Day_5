bf751c0c1f5d7b26b663381fe28870b114960c08
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-20 10:57:37 +0100

    Added instructions to mocha unit tests file

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -1,3 +1,18 @@
+
+// ###Unit Tests
+//
+// You need to write unit tests to ensure that the majority of functions in the calculator operate correctly. The test framework to be used is Mocha. The file to write in is in `/tests/unit/calculator_spec.js`.
+//
+// All of these functions should be tested thoroughly:
+//
+//   - calculator.add()
+//   - calculator.subtract()
+//   - calculator.multiply()
+//   - calculator.divide()
+//   - calculator.numberClick()
+//   - calculator.operatorClick()
+//   - calculator.clearClick()
+
 var Calculator = require('../../public/js/calculator.js')
 var assert = require('assert')
 
