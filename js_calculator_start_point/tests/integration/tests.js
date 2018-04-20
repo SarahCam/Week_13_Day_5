f99cb596cc2291ef21097f55e72f577c7fdf1240
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-20 12:11:41 +0100

    Added selenium test for evaluating decimal numbers ok

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -177,5 +177,53 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4');
   });
 
+// Test for a range of numbers:
+// (positive, negative, decimals, very large numbers)
+  it('should be able to calulate sums that evaluate to a negative number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-2');
+  });
+
+  it('should be able to calulate sums that evaluate to a decimal number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.5');
+  });
+
+  // it('should be able to calulate sums that evaluate to a very large number', function(){
+  //   running_total = element(by.css('#running_total'))
+  //   element(by.css('#number1')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#operator_multiply')).click();
+  //   element(by.css('#number1')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#operator_equals')).click();
+  //   expect(running_total.getAttribute('value')).to.eventually.equal('1e+20');
+  // });
 
 });
