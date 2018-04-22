1c5c13af09b039bce45b2b780f67e1db96e2b796
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-22 21:39:45 +0100

    Added some more unit tests to verify each function

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -29,6 +29,25 @@ describe('calculator', function () {
     assert.equal(25, calculator.runningTotal);
   })
 
+  it('can add - larger numbers', function(){
+    calculator.clearClick();
+    calculator.numberClick(2);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.operatorClick("+");
+    calculator.numberClick(5);
+    calculator.numberClick(5);
+    calculator.numberClick(5);
+    calculator.numberClick(5);
+    calculator.numberClick(5);
+    calculator.numberClick(5);
+    calculator.operatorClick("=");
+    assert.equal(755555, calculator.runningTotal);
+  })
+
   //   - calculator.subtract()
   it('can subtract', function(){
     calculator.clearClick();
@@ -37,6 +56,15 @@ describe('calculator', function () {
     calculator.numberClick(2);
     calculator.operatorClick("=");
     assert.equal(2, calculator.runningTotal);
+  })
+
+  it('can subtract - resulting in negative number', function(){
+    calculator.clearClick();
+    calculator.numberClick(4);
+    calculator.operatorClick("-");
+    calculator.numberClick(9);
+    calculator.operatorClick("=");
+    assert.equal(-5, calculator.runningTotal);
   })
 
   //   - calculator.multiply()
@@ -48,6 +76,23 @@ describe('calculator', function () {
     calculator.operatorClick("=");
     assert.equal(8, calculator.runningTotal);
   })
+
+  it('can multiply -bigger numbers', function(){
+    calculator.clearClick();
+    calculator.numberClick(4);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.operatorClick("*");
+    calculator.numberClick(2);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.operatorClick("=");
+    assert.equal(80000000, calculator.runningTotal);
+  })
+
   //   - calculator.divide()
   it('can divide', function(){
     calculator.clearClick();
@@ -57,20 +102,107 @@ describe('calculator', function () {
     calculator.operatorClick("=");
     assert.equal(4, calculator.runningTotal);
   })
+
+  it('can divide - and display a decimal result', function(){
+    calculator.clearClick();
+    calculator.numberClick(9);
+    calculator.operatorClick("/");
+    calculator.numberClick(4);
+    calculator.operatorClick("=");
+    assert.equal(2.25, calculator.runningTotal);
+  })
+
   //   - calculator.numberClick()
-  it('can click on a number', function(){
+  it('can click on a number - 0', function(){
+    calculator.clearClick();
+    calculator.numberClick(0);
+    assert.equal(0, calculator.runningTotal);
+  })
+
+  it('can click on a number - 1', function(){
+    calculator.clearClick();
+    calculator.numberClick(1);
+    assert.equal(1, calculator.runningTotal);
+  })
+
+  it('can click on a number - 2', function(){
+    calculator.clearClick();
+    calculator.numberClick(2);
+    assert.equal(2, calculator.runningTotal);
+  })
+
+  it('can click on a number - 3', function(){
+    calculator.clearClick();
+    calculator.numberClick(3);
+    assert.equal(3, calculator.runningTotal);
+  })
+
+  it('can click on a number - 4', function(){
+    calculator.clearClick();
+    calculator.numberClick(4);
+    assert.equal(4, calculator.runningTotal);
+  })
+  it('can click on a number - 5', function(){
+    calculator.clearClick();
+    calculator.numberClick(5);
+    assert.equal(5, calculator.runningTotal);
+  })
+  it('can click on a number - 6', function(){
+    calculator.clearClick();
+    calculator.numberClick(6);
+    assert.equal(6, calculator.runningTotal);
+  })
+  it('can click on a number - 7', function(){
+    calculator.clearClick();
+    calculator.numberClick(7);
+    assert.equal(7, calculator.runningTotal);
+  })
+
+  it('can click on a number - 8', function(){
     calculator.clearClick();
     calculator.numberClick(8);
     assert.equal(8, calculator.runningTotal);
   })
+
+  it('can click on a number - 9', function(){
+    calculator.clearClick();
+    calculator.numberClick(9);
+    assert.equal(9, calculator.runningTotal);
+  })
+
   //   - calculator.operatorClick()
-  it('can click on an operator', function(){
+  it('can click on an operator - divide', function(){
     calculator.clearClick();
     calculator.numberClick(8);
     calculator.operatorClick("/");
     assert.equal(8, calculator.runningTotal);
     assert.equal("/", calculator.previousOperator);
   })
+
+  it('can click on an operator - multiply', function(){
+    calculator.clearClick();
+    calculator.numberClick(8);
+    calculator.operatorClick("*");
+    assert.equal(8, calculator.runningTotal);
+    assert.equal("*", calculator.previousOperator);
+  })
+
+  it('can click on an operator - add', function(){
+    calculator.clearClick();
+    calculator.numberClick(8);
+    calculator.operatorClick("+");
+    assert.equal(8, calculator.runningTotal);
+    assert.equal("+", calculator.previousOperator);
+  })
+
+  it('can click on an operator - subtract', function(){
+    calculator.clearClick();
+    calculator.numberClick(8);
+    calculator.operatorClick("-");
+    assert.equal(8, calculator.runningTotal);
+    assert.equal("-", calculator.previousOperator);
+  })
+
   //   - calculator.clearClick()
   it('can click on clear button', function(){
     calculator.clearClick();
